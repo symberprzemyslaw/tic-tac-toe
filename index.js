54f90c65d93333b26ce0b70184265a0bfa89bf7b
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-08-08 09:43:52 +0200

    Added basic function, player vs player

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-document.querySelector('.grid').addEventListener('click', (e) => {
-
-    if(e.target.innerText === 'X'){
-        e.target.textContent = 'O'
-    } else {
+const game = {
+    playerXactive : true,
+    playerOactive : false,
+    array : [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+    ],
+    logic(e) {
+    const num = e.target.dataset.num
+    if(this.playerXactive && !e.target.clicked ){
         e.target.textContent = 'X'
-
+        e.target.clicked = true
+        this.playerXactive = false
+        this.array[num - 1] = 'X'
+    } else if (!e.target.clicked) {
+        e.target.textContent = 'O'
+        e.target.clicked = true
+        this.playerXactive = true
+        this.array[num - 1] = 'O'
     }
+    }
+
+
+}
+
+document.querySelector('.grid').addEventListener('click', e => {
+    game.logic(e)
+    console.log(game.array)
 })
 
 
@@ -14,7 +35,9 @@ document.querySelector('.grid').addEventListener('click', (e) => {
   X  X  X
   X  X  X
 ]
-
+Napisanie logiki zmiany przycisku z X na O za drugim klikniecm
+Napisanie logiki sprawdzania kodu
+utworzenie obiektu i metody ktora nastepnie doda sie do domu
 
 
 
